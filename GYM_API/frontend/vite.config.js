4786ed5fd0f1c1e4688afea3de4d938956990a5c
jnunez2301 <jmoises.n23@gmail.com>
2023-11-21 16:09:37 +0100

    vite.config updated.

--- GYM_API/frontend/vite.config.js
+++ GYM_API/frontend/vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3001
+    port: 3001,
+    proxy: {
+      '/api': 'http://localhost:3000'
+    }
   }
 })
