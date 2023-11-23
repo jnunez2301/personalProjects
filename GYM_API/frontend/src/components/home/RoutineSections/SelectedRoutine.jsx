52064f086800d5cd5ffbfdfc062c8148e6ee52f4
jnunez2301 <jmoises.n23@gmail.com>
2023-11-23 19:58:15 +0100

    working

--- GYM_API/frontend/src/components/home/RoutineSections/SelectedRoutine.jsx
+++ GYM_API/frontend/src/components/home/RoutineSections/SelectedRoutine.jsx
@@ -1,52 +1,52 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom'
-import axios from 'axios';
-export const SelectedRoutine = () => {
-    const { type, name } = useParams();
-    const baseURL = `/api/gym/routine/${type}/${name}`
-    const [data, setData] = useState([]);
-    useEffect(() => {
-        axios
-            .get(baseURL)
-            .then(response => setData(response.data))
-            .catch(error => console.log(error))
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
-    /* const chestExercises = data.filter(d => d.body_part === 'Chest'); */
-    const routineInfo = data.length > 0 ? [data[0]] : [];
+    import { useEffect, useState } from 'react';
+    import { useParams } from 'react-router-dom'
+    import axios from 'axios';
+    export const SelectedRoutine = () => {
+        const { type, name } = useParams();
+        const baseURL = `/api/gym/routine/${type}/${name}`
+        const [data, setData] = useState([]);
+        useEffect(() => {
+            axios
+                .get(baseURL)
+                .then(response => setData(response.data))
+                .catch(error => console.log(error))
+            // eslint-disable-next-line react-hooks/exhaustive-deps
+        }, [])
+        /* const chestExercises = data.filter(d => d.body_part === 'Chest'); */
+        const routineInfo = data.length > 0 ? [data[0]] : [];
 
-    return (
-        <section className='routine-container'>
-            {routineInfo.length > 0 && routineInfo.map(info => (
-                <div className='routine-header' key={info.user_id}>
-                    <h2>{info.routine_name}</h2>
-                    <h5>{info.body_part}</h5>
-                    <p>{info.routine_description}</p>
-                    <img width={'200px'} src={info.routine_img} alt={info.routine_name} />
-                </div>
-            ))}
-            <table className='exercise-table' >
-                <thead>
-                    <tr>
-                        <td>Exercise Name</td>
-                        <td>Description</td>
-                        <td>Video Reference</td>
-                    </tr>
-                </thead>
-                {
-                    data.length > 0 && data.map(exercise => (
+        return (
+            <section className='routine-container'>
+                {routineInfo.length > 0 && routineInfo.map(info => (
+                    <div className='routine-header' key={info.user_id}>
+                        <h2>{info.routine_name}</h2>
+                        <h5>{info.body_part}</h5>
+                        <p>{info.routine_description}</p>
+                        <img width={'200px'} src={info.routine_img} alt={info.routine_name} />
+                    </div>
+                ))}
+                <table className='exercise-table' >
+                    <thead>
+                        <tr>
+                            <td>Exercise Name</td>
+                            <td>Description</td>
+                            <td>Video Reference</td>
+                        </tr>
+                    </thead>
+                    {
+                        data.length > 0 && data.map(exercise => (
 
-                        <tbody key={exercise.exercise_id}>
-                            <tr>
-                                <td>{exercise.exercise_name}</td>
-                                <td>{exercise.description}</td>
-                                <td><a href={exercise.youtubeSrc} target='_blank' rel="noreferrer" >Video</a></td>
-                            </tr>
-                        </tbody>
+                            <tbody key={exercise.exercise_id}>
+                                <tr>
+                                    <td>{exercise.exercise_name}</td>
+                                    <td>{exercise.description}</td>
+                                    <td><a href={exercise.youtubeSrc} target='_blank' rel="noreferrer" >Video</a></td>
+                                </tr>
+                            </tbody>
 
-                    ))
+                        ))
 
-                }</table>
-        </section>
-    )
-}
+                    }</table>
+            </section>
+        )
+    }
--- GYM_API/frontend/src/components/home/RoutineSections/Weights.jsx
+++ GYM_API/frontend/src/components/home/RoutineSections/Weights.jsx
@@ -1,5 +1,34 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom";
+
 export const Weights = () => {
+  const baseUrl = '/api/gym/routines/1'
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+    .get(baseUrl)
+    .then(response => setData(response.data))
+    .catch(error => console.log(error))
+  },[])
+  
+  
   return (
-    <div>Weights</div>
+    <section className="routines">
+      {
+        data && data.map(routine => (
+          <Link 
+          className='routine-link'          
+          to={`/routine-name/weights/${routine.routine_alias}`} key={routine.routine_id}>
+              <h2>{routine.routine_name}</h2>
+              <img 
+              width={'200px'} 
+              src={routine.routine_img} alt={`${routine.routine_name}`} />
+              <p>{routine.routine_description}</p>
+
+          </Link>
+        ))
+      }
+    </section>
   )
 }
