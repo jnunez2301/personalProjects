866aaae8e5030d8baaef3542fff71c34fa93c709
jnunez2301 <jmoises.n23@gmail.com>
2023-11-24 19:51:12 +0100

    Update config.js

--- GYM_API/backend/config.js
+++ GYM_API/backend/config.js
@@ -3,8 +3,8 @@ const secret = require('./utils/secret');
 const config = {
     db: {
         host: 'sql8.freemysqlhosting.net',
-        user: secret.username,
-        password: secret.password,
+        user: process.env.MyUSER,
+        password: process.env.MyPassword,
         database: 'sql8664785',
         /* connectTimeout: 60000 */
     },
