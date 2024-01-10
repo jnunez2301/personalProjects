d8cfbfa92165a9b138d9bedf032361c71fe35526
jnunez2301 <jmoises.n23@gmail.com>
2024-01-10 19:27:34 +0100

    Update ProfileScreen.js

--- WEIGHT_TRACKER/screens/ProfileScreen.js
+++ WEIGHT_TRACKER/screens/ProfileScreen.js
@@ -1,13 +1,44 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TextInput } from 'react-native'
 import { useTheme } from '../context/ThemeProvider'
-
+import { userInfo } from '../helpers/UserInfo';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 
 export const ProfileScreen = () => {
-  const { themeBackgroundColor, themeTextColor } = useTheme();
+  const { themeColor, themeTextColor, themeBackgroundColor } = useTheme();
+  
+  
+
   return (
-    <View style={[style.container,{ backgroundColor: themeBackgroundColor}]}>
-        <Text style={{color: themeTextColor}}>HistoryScreen</Text>
+    <View style={[style.container, { backgroundColor: themeBackgroundColor }]}>
+      <View style={[style.inputCardContainer]}>
+        <Text style={{color: themeTextColor, fontSize: 24, marginBottom: 10, fontWeight: 'bold'}}>Settings</Text>
+        <View style={style.inputCard}>
+          <TextInput 
+          keyboardType='numeric'
+          placeholder='Age'
+          textAlign='center'
+          placeholderTextColor={themeTextColor}
+          style={[style.textInput, {borderColor: themeTextColor, color: themeTextColor}]}/>
+          <Ionicons name='calculator' size={30} color={themeTextColor}/>
+        </View>
+        <View style={style.inputCard}>
+          <TextInput placeholder='Height'
+          keyboardType='numeric'
+          placeholderTextColor={themeTextColor}
+          style={[style.textInput, {borderColor: themeTextColor, color: themeTextColor}]}/>
+          <Text style={{color: themeTextColor, 
+          fontWeight: 'bold'}}>cm</Text>
+        </View>
+        <View style={[style.inputCard]}>
+          <TextInput 
+          keyboardType='numeric'
+          placeholder='Target Weight'
+          placeholderTextColor={themeTextColor}
+          style={[style.textInput, {borderColor: themeTextColor, color: themeTextColor}]}/>
+          <Text style={{color: themeTextColor, textAlign: 'center', fontWeight: 'bold'}}>kg</Text>
+        </View>
+      </View>
     </View>
   )
 }
@@ -16,5 +47,24 @@ export const ProfileScreen = () => {
 const style = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 30
+  },
+  textInput: {
+    borderWidth: 1,
+    height: 40,
+    width: 240,
+    marginRight: 10,
+    padding: 5,
+    borderRadius: 5,
+    textAlign: 'center',
+  },
+  inputCard: {
+    flexDirection: 'row',
+    marginBottom: 16,
+    alignItems: 'center'
+  },
+  inputCardContainer: {
+    justifyContent: 'center',
+    
   }
 })
