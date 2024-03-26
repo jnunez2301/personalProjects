866aa0c9a1b8552b5e95d306e897b34ba890e9a7
Jhonatan Nunez <jmoises.n23@gmail.com>
2024-03-26 15:50:36 +0100

    Frontend working

--- codeSharingApp/src/app/app.component.ts
+++ codeSharingApp/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { MessageService } from 'primeng/api';
 import { DropdownChangeEvent } from 'primeng/dropdown';
+import {Clipboard} from '@angular/cdk/clipboard';
+
 
 @Component({
   selector: 'app-root',
@@ -23,14 +25,24 @@ export class AppComponent {
     { name: 'Typescript', code: 'typescript' },
   ];
 
-  constructor(private messageService: MessageService) {}
+  constructor(private messageService: MessageService, private clipboard: Clipboard) {}
 
-  showSuccess() {
+  showSuccessShare() {
     this.messageService.add({
       severity: 'success',
-      summary: 'Success',
-      detail: 'Message Content',
+      summary: 'URL copied',
+      detail: 'Now you can share the code with anyone',
     });
+    this.clipboard.copy(window.location.href)
+  }
+
+  showInfoCopy() {
+    this.messageService.add({
+      severity: 'info',
+      summary: 'Copied to clipboard',
+      detail: 'The code is on your clipboard now',
+    });
+    this.clipboard.copy(this.code)
   }
 
   editorOptions = {
