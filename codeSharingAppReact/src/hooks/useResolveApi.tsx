367902596582449670d38e9c210e7f578ba19643
Jhonatan Nunez <jmoises.n23@gmail.com>
2024-03-29 23:19:18 +0100

    API CRUD (Missing delete)

--- codeSharingAppReact/src/hooks/useResolveApi.tsx
+++ codeSharingAppReact/src/hooks/useResolveApi.tsx
@@ -1,22 +1,49 @@
 import axios from "axios";
+import { SharedCode } from "../models/SharedCode";
 
 export const useResolveAPi = () => {
-    const baseURL = import.meta.env.VITE_BASE_URL;
-    const getCodes = () => {
-        console.log(baseURL);
-        
-        axios
-        .get(baseURL)
-        .then(response =>  {
-            console.log(response);
-            
-            return response.data
-        })
-        .catch(error => {
-            console.log(error);
-        })
-    }
-    return {
-        getCodes
-    }
-}
+  const baseURL = import.meta.env.VITE_BASE_URL;
+  const getCodes = () => {
+    axios
+      .get(baseURL)
+      .then((response) => {
+        return response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const getCodeById = (idParam: string) => {
+    axios
+    .get(`${baseURL}/${idParam}`)
+    .then(response => {
+      return response.data
+    })
+    .catch(error => console.log(error))
+  }
+  const postCodes = (idParam: string) => {
+    axios
+    .post(`${baseURL}/${idParam}`)
+    .then((response) => {
+      return {data: response.data, status: response.status};
+    })
+    .catch(error => console.log(error))
+  }
+  const updateCode = (newCode: SharedCode) => {
+    axios
+    .post(baseURL, newCode)
+    .then(response => {
+      return {
+        data: response.data,
+        status: response.status
+      }
+    })
+    .catch(error => console.log(error))
+  }
+  return {
+    getCodes,
+    getCodeById,
+    postCodes,
+    updateCode
+  };
+};
