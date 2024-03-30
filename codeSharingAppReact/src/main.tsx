84ff2834be74c6ea6c9613930ce7eaa661045a29
Jhonatan Nunez <jmoises.n23@gmail.com>
2024-03-30 14:30:28 +0100

    Devtools removed

--- codeSharingAppReact/src/main.tsx
+++ codeSharingAppReact/src/main.tsx
@@ -5,7 +5,6 @@ import 'primeicons/primeicons.css';
 import { PrimeReactProvider } from 'primereact/api';
 import "primereact/resources/themes/lara-light-blue/theme.css";
 import {  QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import Routes from './routes/Routes.tsx';
 
 
@@ -16,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
     <PrimeReactProvider>
       <Routes />
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </PrimeReactProvider>
     </QueryClientProvider>
   </React.StrictMode>,
